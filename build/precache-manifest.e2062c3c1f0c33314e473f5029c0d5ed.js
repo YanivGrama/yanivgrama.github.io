053eb8afb949c03108653c8029be545c2251349f
YanivGrama <yaniv.grama@logz.io>
2019-09-09 22:19:36 +0300

    add pages

---
+++ build/precache-manifest.e2062c3c1f0c33314e473f5029c0d5ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e56e239fb2c5a7004ecc69c645cce88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74b3eed1e0e9872aa503",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "7cb5f86aa03d90c03c10",
+    "url": "/static/js/2.aeca51ce.chunk.js"
+  },
+  {
+    "revision": "74b3eed1e0e9872aa503",
+    "url": "/static/js/main.a7b5c79c.chunk.js"
+  },
+  {
+    "revision": "c48f7c90d79570720fb4",
+    "url": "/static/js/runtime~main.809033da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
